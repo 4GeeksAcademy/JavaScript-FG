e5eb3e762291d367e7066d54e97db67a5edd9a88
GeFernando <164933481+GeFernando@users.noreply.github.com>
2024-05-01 01:04:26 +0000

    excusas-alineaciones

--- src/app.js
+++ src/app.js
@@ -8,4 +8,29 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = excuseGen();
 };
+
+function excuseGen() {
+  let who = [
+    "El mentor H.Q.",
+    "Camila Fernandez",
+    "Mi exorcista",
+    "La Anyhara"
+  ];
+  let action = ["bailo", "bebio", "estuvo", "boxeo"];
+  let what = ["con los nazis", "junto al Papa Benedicto", "con Marlon Brando"];
+  let when = [
+    "por consecuencia no fue a la clase del lunes",
+    "cuando el Franco fue nombrado ministro de los pokemones",
+    "mientras tomaba votos de castidad",
+    "durante el nacimiento de una nueva constelacion",
+    "en tanto el alcohol hablara en el nombre del amor"
+  ];
+  let who1 = Math.floor(Math.random() * who.length);
+  let action1 = Math.floor(Math.random() * action.length);
+  let what1 = Math.floor(Math.random() * what.length);
+  let when1 = Math.floor(Math.random() * when.length);
+
+  return `${who[who1]} ${action[action1]} ${what[what1]} ${when[when1]}`;
+}
