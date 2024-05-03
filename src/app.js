72fa48fada5274cf6ba79d39db27341e74c6efa6
GeFernando <164933481+GeFernando@users.noreply.github.com>
2024-05-03 23:45:08 +0000

    refactorizacion

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
@@ -18,8 +16,11 @@ function excuseGen() {
     "Mi exorcista",
     "La Anyhara"
   ];
+
   let action = ["bailo", "bebio", "estuvo", "boxeo"];
+
   let what = ["con los nazis", "junto al Papa Benedicto", "con Marlon Brando"];
+
   let when = [
     "por consecuencia no fue a la clase del lunes",
     "cuando el Franco fue nombrado ministro de los pokemones",
@@ -27,10 +28,11 @@ function excuseGen() {
     "durante el nacimiento de una nueva constelacion",
     "en tanto el alcohol hablara en el nombre del amor"
   ];
-  let who1 = Math.floor(Math.random() * who.length);
-  let action1 = Math.floor(Math.random() * action.length);
-  let what1 = Math.floor(Math.random() * what.length);
-  let when1 = Math.floor(Math.random() * when.length);
 
-  return `${who[who1]} ${action[action1]} ${what[what1]} ${when[when1]}`;
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  return `${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
 }
